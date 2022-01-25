fff645cea04a2d49da07063da30436ef9efe3cf2
Qazaroth <realthedarksid3r@gmail.com>
2022-01-25 20:43:54 +0800

    Added recaptcha v2

---
+++ Models/MyObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AS_Assignment.Models
+{
+    public class MyObject
+    {
+        public string success { get; set; }
+        public List<String> errorMessages { get; set; }
+    }
+}
--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AS_Assignment.Pages.Shared
 {
@@ -22,6 +27,8 @@ namespace AS_Assignment.Pages.Shared
         public String password { get; set; }
         [BindProperty]
         public String error { get; set; }
+        [BindProperty]
+        public object e { get; set; }
 
         private readonly UserService _ctx;
 
@@ -36,36 +43,59 @@ namespace AS_Assignment.Pages.Shared
 
         public IActionResult OnPost()
         {
-            string p = this.password;
-            string e = this.email;
-
-            SHA512 hashing = SHA512.Create();
-            string hash = getDBHash(email);
-            string salt = getDBSalt(email);
-
-            if (salt != null && salt.Length > 0 && hash != null && hash.Length > 0)
+            if (CaptchaPassed())
             {
-                string pwdWithSalt = p + salt;
-                byte[] hashWithSalt = hashing.ComputeHash(Encoding.UTF8.GetBytes(pwdWithSalt));
-                string userHash = Convert.ToBase64String(hashWithSalt);
+                string p = this.password;
+                string e = this.email;
 
-                if (userHash.Equals(hash))
+                SHA512 hashing = SHA512.Create();
+                string hash = getDBHash(email);
+                string salt = getDBSalt(email);
+
+                if (salt != null && salt.Length > 0 && hash != null && hash.Length > 0)
                 {
-                    User u = getUser(email);
+                    string pwdWithSalt = p + salt;
+                    byte[] hashWithSalt = hashing.ComputeHash(Encoding.UTF8.GetBytes(pwdWithSalt));
+                    string userHash = Convert.ToBase64String(hashWithSalt);
 
-                    HttpContext.Session.SetInt32("SSID", u.id);
-                    HttpContext.Session.SetString("SSEmail", u.email);
+                    if (userHash.Equals(hash))
+                    {
+                        User u = getUser(email);
 
-                    return RedirectToPagePermanent("/Index");
+                        HttpContext.Session.SetInt32("SSID", u.id);
+                        HttpContext.Session.SetString("SSEmail", u.email);
+
+                        return RedirectToPagePermanent("/Index");
+                    }
+                    else
+                    {
+                        error = "Invalid account details. Please try again!";
+                        return Page();
+                    }
                 }
-                else
-                {
-                    error = "Invalid account details. Please try again!";
-                    return Page();
-                }
+
+                return Page();
             }
-            
-            return Page();
+            else
+                return Page();
+        }
+
+        public bool CaptchaPassed()
+        {
+            HttpClient c = new HttpClient();
+
+            var res = c.GetAsync($"https://www.google.com/recaptcha/api/siteverify?secret=secret key&response={Request.Form["g-recaptcha-response"]}").Result;
+
+            if (res.StatusCode != HttpStatusCode.OK)
+                return false;
+
+            string JsonRes = res.Content.ReadAsStringAsync().Result;
+            dynamic Jsondata = JObject.Parse(JsonRes);
+
+            if (Jsondata.success != "true" || Jsondata.score <= 0.5m)
+                return false;
+
+            return true;
         }
 
         protected User getUser(String email)
